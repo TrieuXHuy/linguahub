2677a542552c476d8d8f0ea16ecb644b97fbfd2c
TrieuHuy <trieuxuanhuytq3105@gmail.com>
2025-10-11 11:14:20 +0700

    Initial commit

---
+++ src/main/java/com/huy/linguahub/LinguahubApplication.java
@@ -0,0 +1,13 @@
+package com.huy.linguahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguahubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguahubApplication.class, args);
+	}
+
+}
